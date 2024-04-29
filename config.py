418ff77378dd36c410ced0f76d8048dfb6ad3547
bohrium2b <dzm2026@gmail.com>
2024-04-29 06:31:30 +0000

    feat: Add config file

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+A config file for tabulatevotes.
+"""
+
+# The number of seats to be filled in the election.
+# You might not want to set this to be more than 4, otherwise it stops working.
+SEATS = 2 # type: int
+
+# Choices List File
+CHOICES_FILE = "choices.list" # type: str
+
+# Ballots List File
+BALLOTS_FILE = "ballots.csv" # type: str
