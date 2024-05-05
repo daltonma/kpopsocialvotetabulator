640ea00534c933d98665761812017b3310d7fb79
bohrium2b <dzm2026@gmail.com>
2024-05-05 05:41:33 +0000

    Remove duplicates and show more choices.

--- tabulatevotes.py
+++ tabulatevotes.py
@@ -1,6 +1,7 @@
 import config
 import csv
 import pyrankvote
+from pyrankvote.models import DuplicateCandidatesError
 from pyrankvote import Candidate, Ballot
 from rich import print
 
@@ -10,7 +11,8 @@ def load() -> tuple[list[str], list[dict[str, str]]]:
     """
     # Read choices from choices.list
     with open(config.CHOICES_FILE, 'r') as choices_file:
-        choices = choices_file.read().splitlines()
+        choices_raw = choices_file.read().rstrip().splitlines()
+        choices = [line.rstrip() for line in choices_raw]
 
     # Read ballots from ballots.csv
     with open(config.BALLOTS_FILE, 'r') as ballots_file:
@@ -27,13 +29,19 @@ def tabulatevotes():
     choices: dict[str, Candidate] = {choice: Candidate(choice) for choice in rawchoices}
     ballots = []
     for ballot in rawballots:
-        ballots.append(Ballot(ranked_candidates=[choices[choice] for choice in ballot.values()]))
+        try:
+            ballots.append(Ballot(ranked_candidates=[choices[choice] for choice in ballot.values()]))
+        except DuplicateCandidatesError:
+            print(f"[bold red]DuplicateCandidatesError: {ballot}[/bold red]")
+            continue
     result = pyrankvote.single_transferable_vote(list(choices.values()), ballots, number_of_seats=config.SEATS)
     lastroundwinners = result.rounds[-1]
     print("Results:")
     winners = lastroundwinners.candidate_results
     for winner in winners[0:config.SEATS]:
         print(f"[bold green]🎉 {winner.candidate.name}[/bold green] - {winner.number_of_votes:.2f} votes")
+    for loser in winners[config.SEATS:]:
+        print(f"[bold {'red' if loser.number_of_votes <= 0 else 'yellow'}]  {loser.candidate.name}[/] - {loser.number_of_votes:.2f} votes")
 
 
 if __name__ == "__main__":
